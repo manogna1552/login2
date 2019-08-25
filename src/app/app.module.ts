a0e3eb0835d3b18fe500cbadf3e4f433046ec584
Manogna Madineni <manognamadineni@Manognas-MacBook-Pro.local>
2019-08-24 22:32:30 -0400

    ini

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { Routes, RouterModule } from '@angular/router';
+import { UserPageComponent } from './user-page/user-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    UserPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([{path : "",component:LoginPageComponent}])
   ],
   providers: [],
   bootstrap: [AppComponent]
