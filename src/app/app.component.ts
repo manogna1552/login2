4e75e17f7508162babc789d545b5bffffc9d4b71
Manogna Madineni <manognamadineni@Manognas-MacBook-Pro.local>
2019-08-25 21:52:51 -0400

    fg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
-import { LocationStrategy } from '@angular/common';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  constructor(private location: Location){
-    
-  }
 
 }
