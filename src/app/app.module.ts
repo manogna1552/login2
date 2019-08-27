71e030fd6095470cbdbf015366051bb4289b1f82
SRAVYA <sravs66.sravya@gmail.com>
2019-08-27 10:57:08 -0500

    dashboard ui created - sravya

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,22 +6,25 @@ import { LoginPageComponent } from './login-page/login-page.component';
 import { RouterModule } from '@angular/router';
 import { UserPageComponent } from './user-page/user-page.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { MyMaterialModule } from  './material.module';
 import {FlexLayoutModule} from '@angular/flex-layout';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginPageComponent,
-    UserPageComponent
+    UserPageComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot([{path : "login", component : LoginPageComponent},{path : "user", component : UserPageComponent}]),
     BrowserAnimationsModule,
     MyMaterialModule,
+    MatSidenavModule,
     FlexLayoutModule,
 
   ],
---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, share } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent implements OnInit {
+
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches)
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver) {}
+
+  ngOnInit() {
+  }
+
+}
