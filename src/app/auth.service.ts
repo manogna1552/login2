0c60a8c0dfcdae045a841f5359f28421bc56c070
manogna <manogna.madineni6@gmail.com>
2019-08-28 22:25:11 -0400

    updated service page-manogna
    add fake  api calls and redirection

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import  {Observable} from 'rxjs';
+import {Router} from '@angular/router';
 
 
 
@@ -15,28 +16,49 @@ export class AuthService {
   // url = `http://httpbin.org/post`;
   // json;
   
-  constructor(private http:HttpClient) { 
+  constructor(private http:HttpClient,private router: Router ) { 
     // this.http.post(this.url, this.postData).toPromise().then((data:any) => {
     //   console.log(data);
     //   console.log(data.json.test);
     //   this.json = JSON.stringify(data.json);
     // });
   }
+  // res_email="";
+  // res_password="";
+  // res_id= 0;
+
   getUserDetails(email, password){
   
     const postData = {
       email1: email,
       password2 : password
     };
+<<<<<<< Updated upstream
     return this.http.post(`http://httpbin.org/post`,postData).toPromise().then((data:any) => {
+=======
+    return this.http.post(`https://my-json-server.typicode.com/techsithgit/json-faker-directory/profiles/`,postData)
+    .subscribe((data:any)=>{console.log(data);
+      console.log(data.email1);
+     console.log(data.password2);
+     console.log(data.id);
+     if(data.id == 4){
+      this.router.navigate(["user"]);
+      } else {
+       alert("Invalid credentials");
+      }
+    })
+  }
+  
+    //return this.http.post(`http://httpbin.org/post`,postData).toPromise().then((data:any) => {
+>>>>>>> Stashed changes
       //console.log(data);
       //console.log(data.json.email1);
      // console.log(data.json.password1);
   })
    //subscribe(data=>{
     // console.log(data,"login data from server")})
- 
-    }
+//    }
+   
   }
 
  
